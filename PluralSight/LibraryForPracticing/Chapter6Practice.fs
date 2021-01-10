b625adb96bc171567e6eec47904025863b5d5093
Luke Limpert <65961556+luke-limpert@users.noreply.github.com>
2021-01-09 22:07:16 -0500

    Chapter 6 complete

--- PluralSight/LibraryForPracticing/Chapter6Practice.fs
+++ PluralSight/LibraryForPracticing/Chapter6Practice.fs
@@ -44,3 +44,59 @@ p4.Surname <- "Limpert";;
 
 //This allows you to change the surname and forename of a person
 //Again, this would be much easier to do in records
+//Mutable backing store values with getters and setters
+// to use getters and setters you need the val keyword (validation)
+
+type MutableGetter(forename : string, surname : string) =
+    member val Forename = forename with get, set
+    member val Surname = surname with get, set
+
+// this would allow you to alter the forename and surname as a mutable type
+
+//Doing work in the constructor
+
+open System 
+
+type SafePerson (forename : string, surname : string) =
+    let validateString str = 
+        if String.IsNullOrWhiteSpace str then
+            raise (ArgumentException())
+    do 
+        validateString forename
+        validateString surname
+    member val Forename = forename with get, set
+    member val Surname = surname with get, set
+
+//declaring and implementing interfaces
+type IPerson = 
+    abstract member Forename : string
+    abstract member Surname : string
+    abstract member Fullname : string
+
+type PersonFromInterface(forename : string, surname : string) =
+    let validateString str = 
+        if String.IsNullOrWhiteSpace str then
+            raise (ArgumentException())
+    do
+        validateString forename
+        validateString surname
+
+    interface IPerson with
+        member __.Forename = forename
+        member __.Surname = surname
+        member __.Fullname = sprintf "%s %s" forename surname
+
+// in order to make the Fullname property work you need to cast the instance your dealing with to the interface
+// (person :> IPerson).Fullname
+//:> is the upcasting property and its purpose is to upcast from the person to the IPerson to get access to the Fullname property
+//makes it clear if you are relying on a method of the type or interface and in cases where multiple interfaces are implemented, which one you want to implement
+
+//Calling F# code from C#
+//Both are Common Language Infrastructures (CLI)
+//The next portion of the video unpacks a lot of different things, like adding a C# project, then referencing the project, using nuget to add nunit and renaming
+//We reference the F# Project in the C# project
+//Use the NuGet package manager to add the NUnit package
+//For some reason I couldn't get the C# to recognize the LibraryForPracticing, so I couldn't pull from the chapter6Practice.fs
+//will redo it later under a new project name and without information from the other exercises
+
+
---
+++ PluralSight/PersonTests/PersonTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+//using Chapter6;
+
+
+
+
+
+namespace PersonTests
+{
+    public class PersonTests
+    {
+        [Test]
+        public void Person_Fullname_Is_Correctly_Built()
+        {
+            //var expected = "James Mason";
+            //var sut = new PersonFromInterface("James", "Mason");
+            //var actual = (sut as IPerson).Fullname;
+            //Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
